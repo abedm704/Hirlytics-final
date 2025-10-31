edbaecbe2c802c5de04630a85896477b4a00b3df
abedm704 <abdulrahman.almoghrabi@lau.edu>
2025-10-31 05:22:20 +0300

    Update Dashboard.tsx

--- src/components/admin-page/Dashboard.tsx
+++ src/components/admin-page/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import Sidebar from "./Sidebar";
 import JobsSection from "./JobsSection";
 import StatsCards from "./StatsCards";
